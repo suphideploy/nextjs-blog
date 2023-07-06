493ad2b93a91af54788323626bc09fe10f357192
suphideploy <chingchimplee@gmail.com>
2023-07-06 12:49:37 +0700

    Er stellung von Routing and First Page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "077f0e49c34421e5"; }
+/******/ 		__webpack_require__.h = function() { return "abfd287eaf4a3421"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1216,11 +1216,6 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	!function() {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
---
+++ pages/contact.js
@@ -0,0 +1,5 @@
+export default function Contact() {
+    return (
+        <h1>This is Contact Page</h1>
+    );
+}
